1c8292577b9448f3bdd59f89db0614a953edcfdf
Noumaan Shah <noumaan@smartfission.com>
2018-04-27 11:49:13 -0700

    Added support for {N} 4

--- demo-ng/app/home/home.module.ts
+++ demo-ng/app/home/home.module.ts
@@ -1,12 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 
 @NgModule({
     imports: [
-        NativeScriptModule,
         HomeRoutingModule
     ],
     declarations: [
