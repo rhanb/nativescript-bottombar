acd3ec34abf86af2d2ff80294714b686ccf7722b
rhanb <aniel.jeanbaptiste@gmail.com>
2018-11-28 15:39:14 +0100

    e2e implementation

---
+++ demo-vue/app/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+        <GridLayout columns="*" rows="*, auto">
+            <Label class="message" :text="msg" col="0" row="0"/>
+            <BottomBar row="1">
+                <BottomBarItem badgeBackgroundColor="green" badge="1" icon="ic_home_outline" title="Home 1" checkedIcon="ic_home_filled"></BottomBarItem>
+                <BottomBarItem icon="ic_home_outline" badge="2" title="Home 2" checkedIcon="ic_home_filled"></BottomBarItem>
+                <BottomBarItem icon="ic_home_outline" badge="3" title="Home 3" checkedIcon="ic_home_filled"></BottomBarItem>
+            </BottomBar>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
---
+++ src/e2e/bottombar.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { AppiumDriver, createDriver, SearchOptions } from "nativescript-dev-appium";
+import { assert } from "chai";
+import { runType } from "nativescript-dev-appium/lib/parser";
+
+const isAndroid: Boolean = runType.includes("android");
+
+describe("Bottom bar", () => {
+    const defaultWaitTime = 2000000;
+    let driver: AppiumDriver;
+    let bottomBar;
+    const items  = [];
+
+    before(async () => {
+        driver = await createDriver();
+        driver.defaultWaitTime = defaultWaitTime;
+    });
+
+    after(async () => {
+        await driver.quit();
+        console.log("Quit driver!");
+    });
+
+    afterEach(async function () {
+        if (this.currentTest.state === "failed") {
+            await driver.logTestArtifacts(this.currentTest.title);
+        }
+    });
+
+    it("should find the bottombar", async () => {
+        bottomBar = await driver.findElementByAccessibilityId("bottomBar", SearchOptions.exact);
+        assert.exists(bottomBar);
+    });
+
+    it('should have 3 child', async() => {
+        for (let i = 1; i < 4; i++) {
+            items.push(
+                await driver.findElementByAccessibilityId(`bottomBarItem${i}`, SearchOptions.exact)
+            )
+        }
+        assert.equal(items.length, 3);
+    });
+});
